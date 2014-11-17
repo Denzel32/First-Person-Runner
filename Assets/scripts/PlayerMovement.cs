c31901102ec36ec35fcb1721e90f118b9bd8d612
Denzel <denzel_justin@hotmail.com>
2014-11-17 11:36:12 +0100

    First commit. Includes player movement and respawn

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{	
+	public float jumpSpeed = 50f;
+	public float movementSpeed = 50f;
+	private bool jump = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			if(jump == true)
+			{
+				//rigidbody.AddForce(Vector3.up * jumpSpeed);
+				rigidbody.velocity = new Vector3 (0,10,0);
+
+				jump = false;
+			}
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			//rigidbody.AddForce(Vector3.right * movementSpeed);
+			transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector3.right * - movementSpeed * Time.deltaTime);
+		}
+		//print ("im working");
+	}
+	void OnCollisionEnter(Collision collide)
+	{
+		if(collide.gameObject.tag == "Ground")
+		{
+			jump = true;
+		}
+	}
+}
---
+++ Assets/scripts/PlayerRespawn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour 
+{	
+	public Transform spawnpoint;
+	public Transform mytransform;
+	//public Transform spawnpoint;
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Respawn();
+	}
+	
+	void Respawn()
+	{
+		if(transform.position.y <= -10)
+		{
+
+			Vector3 newposition = transform.position;
+			newposition.x = 2;
+			newposition.y = 2;
+			transform.position = newposition;
+			//Destroy(this.gameObject);
+			//Instantiate(spawnpoint, new Vector3(2,2,0), Quaternion.identity);
+
+		}
+	}
+}
